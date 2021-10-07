a7bd34fa130fd685671c7534634396c269da5ded
Martin Z <martinkapo99@gmail.com>
2021-10-07 14:27:44 +0000

    Update escalar.s

--- escalar.s
+++ escalar.s
@@ -10,17 +10,12 @@ mov ecx ,[ebp+12] ;tamano de vector
 mov edx ,0        ;sirve para recorrer el vector  
 
 ciclo:
-push edx
-push ecx
 
 mov eax,[ebp+8]       ; direccion de memoria del primer valor
 fld dword [eax+ edx ] ; guardo el valor de alguna posicion del vector en la pila FPU
 fld dword [ebp+16]    ; guardo el escalar en la pila FPU
 fmul                  ; st0 = st0 * st1 
 fst dword [eax+edx]   ; guardo el valor de st0 en la posicion de memoria adecuada 
-
-pop ecx
-pop edx
 
 add edx ,4            ; 4bytes es el tamano de un float 
 dec ecx               ; para saber cuando dejo de tener posiciones que recorrer
